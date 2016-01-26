a54746331eedb22501aa0ff7da55a18886f40999
Nicola Pertosa <nicola.pertosa@gmail.com>
2016-01-26 17:43:01 +0100

    Unit test for DBFactory

---
+++ tests/DBFactoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Elixir\Test\DB;
+
+use Elixir\DB\DBFactory;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Description of DBFactoryTest
+ *
+ * @author Nicola Pertosa <nicola.pertosa@gmail.com>
+ */
+class DBFactoryTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $this->assertInstanceOf('Elixir\DB\PDO', DBFactory::create(array('type' => DBFactory::PDO_MYSQL, 
+                                                                         'dbname' => 'elixir', 
+                                                                         'host' => 'localhost', 
+                                                                         'port' => '3306',
+                                                                         'username' => 'root',
+                                                                         'password' => '',
+                                                                         'options' => array())));
+    }
+}
