915927490612ce592c7c6c5df292fb283cd52408
Nicola Pertosa <nicola.pertosa@gmail.com>
2016-01-26 17:29:00 +0100

    Unit test for PDO class

---
+++ tests/PDOTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Elixir\Test\DB;
+
+use Elixir\DB\PDO;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Description of PDOTest
+ *
+ * @author Nicola Pertosa <nicola.pertosa@gmail.com>
+ */
+class PDOTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $connection = new \PDO('mysql:dbname=elixir;host=localhost;port=3306', 'root', '');
+        $pdo = new PDO($connection);
+        
+        $this->assertInstanceOf('Elixir\DB\PDO', $pdo);
+        $this->assertEquals($connection->getAttribute(\PDO::ATTR_DRIVER_NAME), $pdo->getDriver());
+        $this->assertEquals(1, $pdo->exec('INSERT INTO `user` (`id`, `firstname`, `name`) VALUES(NULL, "test firstname", "test name")'));
+        $this->assertCount((int) $pdo->lastInsertId(), $pdo->query('SELECT * FROM `user`')->fetchAll());
+    }
+}
